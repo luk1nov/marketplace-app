6132ee4705e35ed37643645593064aaa9a6e8532
Anton <antoshal@gmail.com>
2023-01-15 01:20:32 +0100

    enable config props, feign client and discovery client

--- product-service/src/main/java/com/lukyanov/itemservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/lukyanov/itemservice/ProductServiceApplication.java
@@ -1,11 +1,18 @@
 package com.lukyanov.itemservice;
 
+import com.lukyanov.itemservice.config.ProductStatusPropertyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableConfigurationProperties(ProductStatusPropertyConfig.class)
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
