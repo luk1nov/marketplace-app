4df1d8f057ae0626e88aeb6e432e2b4f67d399bb
Anton <antoshal@gmail.com>
2023-01-15 01:10:27 +0100

    implemented pagination

--- product-service/src/main/java/com/lukyanov/itemservice/service/BaseService.java
+++ product-service/src/main/java/com/lukyanov/itemservice/service/BaseService.java
@@ -1,9 +1,10 @@
 package com.lukyanov.itemservice.service;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BaseService<T, ID>{
     T findById(ID id);
-    List<T> findAll();
+    Page<T> findAll(Pageable pageable);
     void remove(ID id);
 }
--- product-service/src/main/java/com/lukyanov/itemservice/service/impl/CategoryServiceImpl.java
+++ product-service/src/main/java/com/lukyanov/itemservice/service/impl/CategoryServiceImpl.java
@@ -1,14 +1,19 @@
 package com.lukyanov.itemservice.service.impl;
 
-import com.lukyanov.itemservice.dto.CategoryDto;
+import com.lukyanov.itemservice.dto.RequestCategoryDto;
+import com.lukyanov.itemservice.dto.ResponseCategoryDto;
+import com.lukyanov.itemservice.exception.EntityModifyingException;
 import com.lukyanov.itemservice.mapper.CategoryMapper;
 import com.lukyanov.itemservice.repository.CategoryRepository;
 import com.lukyanov.itemservice.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -17,40 +22,44 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryMapper categoryMapper;
 
     @Override
-    public CategoryDto findById(Long id) {
+    public ResponseCategoryDto findById(Long id) {
         return categoryRepository.findById(id)
                 .map(categoryMapper::categoryToDto)
                 .orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
-    public List<CategoryDto> findAll() {
-        return categoryRepository.findAll().stream()
-                .map(categoryMapper::categoryToDto)
-                .toList();
+    public Page<ResponseCategoryDto> findAll(Pageable pageable) {
+        return categoryRepository.findAll(pageable)
+                .map(categoryMapper::categoryToDto);
     }
 
     @Override
-    public CategoryDto create(CategoryDto categoryDto) {
-        return categoryMapper.categoryToDto(categoryRepository.save(categoryMapper.dtoToCategory(categoryDto)));
+    public ResponseCategoryDto create(RequestCategoryDto requestCategoryDto) {
+        return categoryMapper.categoryToDto(categoryRepository.save(categoryMapper.requestCategoryDtoToCategory(requestCategoryDto)));
     }
 
     @Override
-    public CategoryDto update(Long id, CategoryDto categoryDto) {
-        findById(id);
-        categoryDto.setId(id);
-        return categoryMapper.categoryToDto(categoryRepository.save(categoryMapper.dtoToCategory(categoryDto)));
-    }
-
-    @Override
-    public CategoryDto findByName(String name) {
-        return categoryRepository.findFirstByName(name)
-                .map(categoryMapper::categoryToDto)
-                .orElseThrow(EntityNotFoundException::new);
+    public ResponseCategoryDto update(Long id, RequestCategoryDto requestCategoryDto) {
+        ResponseCategoryDto existCategory = findById(id);
+        Set<Long> allChildCategoryIds = new HashSet<>();
+        fillAllSubcategories(allChildCategoryIds, existCategory);
+        if(allChildCategoryIds.contains(requestCategoryDto.getParentCategoryId())){
+            throw new EntityModifyingException("Can not set child category as parent category");
+        }
+        requestCategoryDto.setId(id);
+        return categoryMapper.categoryToDto(categoryRepository.save(categoryMapper.requestCategoryDtoToCategory(requestCategoryDto)));
     }
 
     @Override
     public void remove(Long id) {
         categoryRepository.deleteById(id);
+    }
+
+    void fillAllSubcategories(Set<Long> categorySet, ResponseCategoryDto responseCategoryDto){
+        for (ResponseCategoryDto category: responseCategoryDto.getSubcategories()) {
+            categorySet.add(category.getId());
+            fillAllSubcategories(categorySet, category);
+        }
     }
 }
