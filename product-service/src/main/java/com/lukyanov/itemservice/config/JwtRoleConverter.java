dbfcb9f36f794721ed5b4745e30de0953362b945
Anton <antoshal@gmail.com>
2023-01-18 23:24:13 +0100

    refactored

--- product-service/src/main/java/com/lukyanov/itemservice/config/JwtRoleConverter.java
+++ product-service/src/main/java/com/lukyanov/itemservice/config/JwtRoleConverter.java
@@ -5,19 +5,19 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class JwtRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     private static final String AUTHORITIES = "authorities";
 
     @Override
-    public Collection convert(Jwt jwt) {
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
         List<String> roles = (List<String>) jwt.getClaims().get(AUTHORITIES);
         if (roles == null || roles.isEmpty()) {
-            return new ArrayList<>();
+            return Collections.emptyList();
         }
-        return roles.stream().map(SimpleGrantedAuthority::new).toList();
+        return roles.stream().map(role -> (GrantedAuthority) new SimpleGrantedAuthority(role)).toList();
     }
 }
