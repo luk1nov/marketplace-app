169344aa85d7700007440e7f19e1b2b85f360504
Anton <antoshal@gmail.com>
2023-01-18 23:23:55 +0100

    implemented specification

--- product-service/src/main/java/com/lukyanov/itemservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/lukyanov/itemservice/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.lukyanov.itemservice.repository;
 
 import com.lukyanov.itemservice.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>,
+        JpaSpecificationExecutor<Product> {
 }
