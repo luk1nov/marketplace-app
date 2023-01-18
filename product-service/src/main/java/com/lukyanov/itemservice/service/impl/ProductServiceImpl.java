9ee9cb461511c0fc8124816ab8f7e78b9ab4c4b8
Anton <antoshal@gmail.com>
2023-01-18 23:21:45 +0100

    refactored and finished

--- product-service/src/main/java/com/lukyanov/itemservice/service/impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/lukyanov/itemservice/service/impl/ProductServiceImpl.java
@@ -1,22 +1,27 @@
 package com.lukyanov.itemservice.service.impl;
 
-import com.lukyanov.itemservice.dto.CategoryDto;
-import com.lukyanov.itemservice.dto.ConditionDto;
-import com.lukyanov.itemservice.dto.RequestProductDto;
-import com.lukyanov.itemservice.dto.ResponseProductDto;
+import com.lukyanov.itemservice.client.UserApiClient;
+import com.lukyanov.itemservice.dto.*;
 import com.lukyanov.itemservice.entity.Product;
 import com.lukyanov.itemservice.mapper.CategoryMapper;
 import com.lukyanov.itemservice.mapper.ConditionMapper;
 import com.lukyanov.itemservice.mapper.ProductMapper;
+import com.lukyanov.itemservice.mapper.ProductStatusMapper;
 import com.lukyanov.itemservice.repository.ProductRepository;
 import com.lukyanov.itemservice.service.CategoryService;
 import com.lukyanov.itemservice.service.ConditionService;
 import com.lukyanov.itemservice.service.ProductService;
+import com.lukyanov.itemservice.service.ProductStatusService;
+import com.lukyanov.itemservice.spec.ProductSearchCriteria;
+import com.lukyanov.itemservice.spec.ProductSpecs;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,35 +29,48 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
     private final ConditionService conditionService;
     private final CategoryService categoryService;
+    private final ProductStatusService productStatusService;
+    private final ProductStatusMapper productStatusMapper;
     private final ProductMapper productMapper;
     private final ConditionMapper conditionMapper;
     private final CategoryMapper categoryMapper;
+    private final UserApiClient userApiClient;
 
     @Override
     public ResponseProductDto findById(Long id) {
-        return productRepository.findById(id).map(productMapper::itemToDto)
+        return productRepository.findById(id).map(productMapper::productToDto)
                 .orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
-    public List<ResponseProductDto> findAll() {
-        return productRepository.findAll().stream()
-                .map(productMapper::itemToDto)
-                .toList();
+    public Page<ResponseProductDto> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable)
+                .map(productMapper::productToDto);
     }
 
     @Override
-    public ResponseProductDto create(RequestProductDto requestProductDto) {
-        return productMapper.itemToDto(productRepository.save(buildItemFromRequestItem(requestProductDto)));
+    public ResponseProductDto create(RequestProductDto requestProductDto, String authorizationHeader) {
+        UserDto currentUser = userApiClient.getUserByAuth(authorizationHeader);
+        Product product = buildItemFromRequestItem(requestProductDto);
+        product.setSellerId(currentUser.getId());
+        return productMapper.productToDto(productRepository.save(product));
     }
 
     @Override
     public ResponseProductDto update(Long id, RequestProductDto requestProductDto) {
-//        findById(uuid);
-//        requestItemDto.setId(uuid);
-//        return itemMapper.itemToDto(itemRepository.save(itemMapper.dtoToItem(requestItemDto)));
-        //todo
-        return null;
+        findById(id);
+        Product updatedProduct = buildItemFromRequestItem(requestProductDto);
+        updatedProduct.setId(id);
+        return productMapper.productToDto(productRepository.save(updatedProduct));
+    }
+
+    @Override
+    public Page<ResponseProductDto> findAllWithParams(Pageable pageable, ProductSearchCriteria criteria) {
+        Optional<Specification<Product>> productsByCriteria = ProductSpecs.getProductsByCriteria(criteria);
+        return productsByCriteria.isEmpty() ?
+                findAll(pageable) :
+                productRepository.findAll(productsByCriteria.get(), pageable)
+                        .map(productMapper::productToDto);
     }
 
     @Override
@@ -61,11 +79,12 @@ public class ProductServiceImpl implements ProductService {
     }
 
     private Product buildItemFromRequestItem(RequestProductDto requestProductDto){
-        CategoryDto categoryDto = categoryService.findByName(requestProductDto.getCategory());
+        ResponseCategoryDto responseCategoryDto = categoryService.findById(requestProductDto.getCategoryId());
         ConditionDto conditionDto = conditionService.findByName(requestProductDto.getCondition());
-        Product product = productMapper.dtoToItem(requestProductDto);
-        product.setCategory(categoryMapper.dtoToCategory(categoryDto));
+        Product product = productMapper.dtoToProduct(requestProductDto);
+        product.setCategory(categoryMapper.responseCategoryDtoToCategory(responseCategoryDto));
         product.setCondition(conditionMapper.dtoToCondition(conditionDto));
+        product.setStatus(productStatusMapper.dtoToProductStatus(productStatusService.getDefaultStatus()));
         return product;
     }
 }
