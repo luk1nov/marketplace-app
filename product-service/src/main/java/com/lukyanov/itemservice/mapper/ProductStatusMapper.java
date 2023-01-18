717539bb2b0e2582388c0df1992bb63b60112264
Anton <antoshal@gmail.com>
2023-01-18 23:23:43 +0100

    refactored mappers

--- product-service/src/main/java/com/lukyanov/itemservice/mapper/ProductStatusMapper.java
+++ product-service/src/main/java/com/lukyanov/itemservice/mapper/ProductStatusMapper.java
@@ -3,11 +3,13 @@ package com.lukyanov.itemservice.mapper;
 import com.lukyanov.itemservice.dto.ProductStatusDto;
 import com.lukyanov.itemservice.entity.ProductStatus;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ProductStatusMapper {
 
+    @Mapping(target = "products", ignore = true)
     ProductStatus dtoToProductStatus(ProductStatusDto dto);
 
     ProductStatusDto productStatusToDto(ProductStatus productStatus);
